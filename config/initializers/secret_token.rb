d14ed87c516c6a397e612dcdc0d6fb81bd01f749
TheOtherZach <briggszj@gmail.com>
2013-03-09 17:49:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasx::Application.config.secret_key_base = '0b259032db89889d91de1e91cfec92d041a66d14d6e5d42191b2514be882ca27a631cb141887419981eb81c676a85a06ff6ab6a796e5ad80990fb4e430f7efe8'
