25405f9cd336f7579236a521f99a9931063905d3
TheOtherZach <briggszj@gmail.com>
2013-03-12 21:47:06 -0400

    front end hooked up to due date logig

--- app/controllers/due_dates_controller.rb
+++ app/controllers/due_dates_controller.rb
@@ -1,16 +1,6 @@
 class DueDatesController < ApplicationController
   def index
     @due_dates = []
-    @due_dates << DueDate.new
-  end
-
-  DueDate = Struct.new(:name, :next) do
-    def name
-      "Monthly Payroll"
-    end
-
-    def next
-      "03-15-2013"
-    end
+    @due_dates << DueDate.new(name: "Monthly Payroll", frequency: :monthly, recur_on: [15])
   end
 end
--- spec/features/important_dates_spec.rb
+++ spec/features/important_dates_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 
 feature 'Important dates' do
   scenario "are viewable in list format" do
+    Date.stub(:today) { Date.new(2013, 3, 11) }
     visit '/dates'
-    Date.stub(:today) { Date.parse("03-11-2013") }
     expect(page).to have_content("Dates")
     expect(page).to have_content("03-15-2013")
   end
 
   scenario "update properly" do
-    pending "use real Date data"
+    Date.stub(:today) { Date.new(2013, 5, 16) }
     visit '/dates'
-    Date.stub(:today) { Date.parse("05-16-2013") }
     expect(page).to have_content("06-16-2013")
   end
 
